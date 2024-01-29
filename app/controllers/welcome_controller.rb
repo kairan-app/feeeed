8fa6b86a9294d678c5dbddca6ab62bc6b3b0eb58
june29 <june29.jp@gmail.com>
2024-01-29 21:45:40 +0900

    処理の大枠はこんな感じか？

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @channels = Channel.order(id: :desc).limit(25)
   end
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -6,4 +6,28 @@ class Channel < ApplicationRecord
   validates :site_link, presence: true, length: { maximum: 255 }
   validates :feed_link, presence: true, length: { maximum: 255 }, uniqueness: true
   validates :image_url, presence: true, length: { maximum: 255 }
+
+  class << self
+    def add(url)
+      feed_urls = Feedbag.find(url)
+
+      return nil if feed_urls.empty?
+
+      feed = Feedjira.parse(Faraday.get(feed_urls.first).body)
+      save_from(feed)
+    end
+
+    def save_from(feed)
+      case feed
+      when Feedjira::Parser::RSS
+        save_from_rss(feed)
+      when Feedjira::Parser::Atom
+        save_from_atom(feed)
+      when Feedjira::Parser::ITunesRSS
+        save_from_itunes_rss(feed)
+      when Feedjira::Parser::AtomYoutube
+        save_from_atom_youtube(feed)
+      end
+    end
+  end
 end
