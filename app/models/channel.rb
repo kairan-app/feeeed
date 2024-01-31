41e2e6755ffe09e5888cf083361afe918b67f4c5
june29 <june29.jp@gmail.com>
2024-02-01 00:38:11 +0900

    Channelのデータ組み立て処理を実装していく

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -5,29 +5,72 @@ class Channel < ApplicationRecord
   validates :description, length: { maximum: 255 }
   validates :site_link, presence: true, length: { maximum: 255 }
   validates :feed_link, presence: true, length: { maximum: 255 }, uniqueness: true
-  validates :image_url, presence: true, length: { maximum: 255 }
+  validates :image_url, length: { maximum: 255 }
 
   class << self
     def add(url)
-      feed_urls = Feedbag.find(url)
+      feed_url = Feedbag.find(url).first
 
-      return nil if feed_urls.empty?
+      return nil if feed_url.nil?
 
-      feed = Feedjira.parse(Faraday.get(feed_urls.first).body)
-      save_from(feed)
+      feed = Feedjira.parse(Faraday.get(feed_url).body)
+      save_from(feed, feed_url)
     end
 
-    def save_from(feed)
-      case feed
-      when Feedjira::Parser::RSS
-        save_from_rss(feed)
-      when Feedjira::Parser::Atom
-        save_from_atom(feed)
-      when Feedjira::Parser::ITunesRSS
-        save_from_itunes_rss(feed)
-      when Feedjira::Parser::AtomYoutube
-        save_from_atom_youtube(feed)
-      end
+    def save_from(feed, feed_url)
+      parameters =
+        case feed
+        when Feedjira::Parser::RSS
+          build_from_rss(feed)
+        when Feedjira::Parser::Atom
+          build_from_atom(feed)
+        when Feedjira::Parser::ITunesRSS
+          build_from_itunes_rss(feed)
+        when Feedjira::Parser::AtomYoutube
+          build_from_atom_youtube(feed)
+        end
+
+      Channel.create(parameters.merge(feed_link: feed_url))
+    end
+
+    def build_from_rss(feed)
+      p "save_from_rss"
+      og = OpenGraph.new(feed.url)
+      {
+        title: feed.title,
+        description: feed.description,
+        site_link: feed.url,
+        image_url: og.images.first,
+      }
+    end
+
+    def build_from_atom(feed)
+      p "save_from_atom"
+      og = OpenGraph.new(feed.url)
+      {
+        title: feed.title,
+        description: feed.description,
+        site_link: feed.links.first,
+        image_url: og.images.first,
+      }
+    end
+
+    def build_from_itunes_rss(feed)
+      p "save_from_itunes_rss"
+      {
+        title: feed.title,
+        description: feed.description,
+        site_link: feed.url,
+        image_url: feed.itunes_image,
+      }
+    end
+
+    def build_from_atom_youtube(feed)
+      p "save_from_atom_youtube"
+      {
+        title: feed.title,
+        site_link: feed.url,
+      }
     end
   end
 end
