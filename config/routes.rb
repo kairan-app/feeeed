3e852201ea0604545e419e0fed663ed97709d0c4
june29 <june29.jp@gmail.com>
2024-02-02 20:27:02 +0900

    Item情報を取得して保存する

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   post "channels", to: "channels#create"
+  get "channels/:channel_id", to: "channels#show", as: "channel"
 
   root "welcome#index"
 end
