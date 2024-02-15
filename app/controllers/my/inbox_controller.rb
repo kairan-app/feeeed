14e5ec03941171a93a639e1c30c92aecd2aa7b3d
june29 <june29.jp@gmail.com>
2024-02-15 22:21:39 +0900

    Inboxページにtitleを設定する

--- app/controllers/my/inbox_controller.rb
+++ app/controllers/my/inbox_controller.rb
@@ -1,5 +1,7 @@
 class My::InboxController < MyController
   def show
     @items = current_user.subscribed_items.preload(:channel).order("items.published_at DESC").page(params[:page])
+
+    @title = "Inbox"
   end
 end
