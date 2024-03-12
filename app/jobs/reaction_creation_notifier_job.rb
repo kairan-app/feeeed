0fda8296d4bf14824b90eb6797690e69651518e6
june29 <june29.jp@gmail.com>
2024-03-12 23:31:32 +0900

    ReactionCreationNotifierJobでもembedsを活用する

--- app/jobs/reaction_creation_notifier_job.rb
+++ app/jobs/reaction_creation_notifier_job.rb
@@ -7,13 +7,18 @@ class ReactionCreationNotifierJob < ApplicationJob
     user = reaction.user
     item = reaction.item
 
-    content = ["@#{user.name} reacted to #{item.url}", reaction.memo].compact.join("\n")
+    content = "@#{user.name} pawed!"
+    embeds = [
+      {
+        title: [item.title, item.channel.title].join(" | "),
+        description: reaction.memo.present? ? "💬 #{reaction.memo}" : nil,
+        url: item.url,
+        thumbnail: { url: item.image_url_or_placeholder },
+      }
+    ]
   
     Faraday.post(
-      webhook_url, {
-        content: "[#{Rails.env}]\n#{content}"
-      }.to_json,
-      "Content-Type" => "application/json"
+      webhook_url, { content: "[#{Rails.env}] #{content}", embeds: }.to_json, "Content-Type" => "application/json"
     )
   end
 end
