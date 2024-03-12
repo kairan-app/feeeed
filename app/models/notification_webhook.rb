0bf9ee99085ec07a046edee96339c61bdb768a64
june29 <june29.jp@gmail.com>
2024-03-12 23:13:55 +0900

    簡易なDiscord通知を実装する

--- app/models/notification_webhook.rb
+++ app/models/notification_webhook.rb
@@ -3,4 +3,28 @@ class NotificationWebhook < ApplicationRecord
 
   validates :user_id, presence: true
   validates :url, presence: true, length: { maximum: 2083 }
+
+  def notify_reactions(recent: 24.hours.ago)
+    reactions = user.reactions.where("created_at > ?", recent)
+    return if reactions.empty?
+
+    reactions_text =
+      reactions.map { |reaction|
+        string = "- #{reaction.item.title} | #{reaction.item.channel.title}\n  - #{reaction.item.url}"
+        string += "\n  - 💬 #{reaction.memo}" if reaction.memo.present?
+        string
+      }.join("\n")
+
+    message = <<~MESSAGE
+      @#{user.name}'s recent pawprints 🐾
+      #{reactions_text}
+    MESSAGE
+
+    # https://discord.com/developers/docs/resources/channel#message-object-message-flags
+    flags = "01000000100".to_i(2)
+
+    Faraday.post(
+      url, { content: message, flags: flags }.to_json, "Content-Type" => "application/json"
+    )
+  end
 end
