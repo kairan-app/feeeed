dedff7321088df58780f240ce3c44ef992adca01
hiromisugie <lozz.gggg@gmail.com>
2024-03-20 14:50:18 +0900

    トップページにRecent Pawprintsを追加

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @channels = Channel.order(id: :desc).limit(12)
     @items = Item.preload(:pawprints).eager_load(:channel).order(published_at: :desc, title: :desc).limit(12)
+    @pawprints = Pawprint.order(id: :desc).limit(12)
 
     @title = "Feed Network"
   end
