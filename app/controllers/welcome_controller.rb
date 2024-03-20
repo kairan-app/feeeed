0bbb5c2907a30c771770ecb5a316124b0ad63728
hiromisugie <lozz.gggg@gmail.com>
2024-03-20 16:04:15 +0900

    トップページのレイアウト調整に伴い表示件数を調整

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @channels = Channel.order(id: :desc).limit(12)
+    @channels = Channel.order(id: :desc).limit(10)
     @items = Item.preload(:pawprints).eager_load(:channel).order(published_at: :desc, title: :desc).limit(12)
-    @pawprints = Pawprint.order(id: :desc).limit(12)
+    @pawprints = Pawprint.order(id: :desc).limit(10)
 
     @title = "Feed Network"
   end
