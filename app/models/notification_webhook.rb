63b8451bfb96dc75113550012b32f407b5e3eb2e
june29 <june29.jp@gmail.com>
2024-03-23 22:01:10 +0900

    購読しているChannelの新着Itemを通知するモードを実装する

--- app/models/notification_webhook.rb
+++ app/models/notification_webhook.rb
@@ -12,19 +12,45 @@ class NotificationWebhook < ApplicationRecord
 
   class << self
     def notify
-      find_each { _1.notify_pawprints }
+      find_each(&:notify)
+    end
+  end
+
+  def notify
+    case mode
+    when "my_subscribed_items"
+      notify_subscribed_items
+    when "my_pawprints"
+      notify_pawprints
     end
   end
 
   def notify_pawprints(since: nil)
     at = since || last_notified_at || 6.hours.ago
-    pawprints = user.pawprints.where("created_at >= ?", at).order(id: :desc)
+    pawprints = user.pawprints.where("created_at >= ?", at).order(:id)
     return if pawprints.empty?
 
-    content = "@#{user.name}'s recent pawprints 🐾"
-
-    pawprints.find_in_batches(batch_size: 10) { |sub_pawprints|
+    pawprints.to_a.each_slice(3).with_index { |sub_pawprints, index|
+      content = "@#{user.name}'s recent pawprints 🐾" if index == 0
       embeds = sub_pawprints.map(&:to_embed)
+
+      sleep 2
+      Faraday.post(
+        url, { content:, embeds: }.to_json, "Content-Type" => "application/json"
+      )
+    }
+
+    touch(:last_notified_at)
+  end
+
+  def notify_subscribed_items(since: nil)
+    at = since || last_notified_at || 6.hours.ago
+    items = user.subscribed_items.where("items.created_at >= ?", at).order("items.id")
+    return if items.empty?
+
+    items.to_a.each_slice(3).with_index { |sub_items, index|
+      content = "Recent items in @#{user.name}'s subscribed channels 📨" if index == 0
+      embeds = sub_items.map(&:to_embed)
 
       sleep 2
       Faraday.post(
