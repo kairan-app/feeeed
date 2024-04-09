6b3a0a1596b1e69c0b99a06f72e05ad63db804bc
hiromisugie <lozz.gggg@gmail.com>
2024-04-09 16:35:20 +0900

    トップページにChannel&Itemsコンポーネントを実装しようと模索（未遂）

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    @channels = Channel.order(id: :desc).limit(10)
-    @items = Item.preload(:pawprints).eager_load(:channel).order(published_at: :desc, title: :desc).limit(12)
-    @pawprints = Pawprint.order(id: :desc).limit(15)
+    @channels = Channel.includes(:items).limit(10)
+    @channel_and_items_for_welcome = @channels.map do |channel|
+      [channel, channel.items.order(created_at: :desc).limit(3)]
+    end.sort_by { |channel, items| items.first&.created_at }.reverse.to_h
+    @pawprints = Pawprint.order(id: :desc).limit(30)
 
     @title = "Feed Network"
   end
