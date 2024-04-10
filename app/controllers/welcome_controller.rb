e849020db56d4771b80c6bfdce5bc86f3f081413
juneboku <juneboku@gmail.com>
2024-04-10 13:31:48 +0900

    ChannelとItemsをズバッと取得する処理を書く

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-    @channels = Channel.includes(:items).limit(10)
-    @channel_and_items_for_welcome = @channels.map do |channel|
-      [channel, channel.items.order(created_at: :desc).limit(3)]
-    end.sort_by { |channel, items| items.first&.created_at }.reverse.to_h
-    @pawprints = Pawprint.order(id: :desc).limit(30)
+    @channels =
+      Channel.
+        joins(:items).
+        select("channels.*, MAX(items.id) AS max_item_id").
+        group("channels.id").
+        order("max_item_id DESC").
+        limit(30)
 
-    @title = "Feed Network"
+    @title = "Enjoy feeds!"
   end
 end
