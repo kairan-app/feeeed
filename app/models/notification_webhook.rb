8b05f9ffca9866b3f2c28a497427851de537c4db
juneboku <juneboku@gmail.com>
2024-04-14 23:14:14 +0900

    Slack向け新着Item通知を実装する

--- app/models/notification_webhook.rb
+++ app/models/notification_webhook.rb
@@ -1,4 +1,6 @@
 class NotificationWebhook < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   belongs_to :user
 
   validates :user_id, presence: true
@@ -19,7 +21,11 @@ class NotificationWebhook < ApplicationRecord
   def notify
     case mode
     when "my_subscribed_items"
-      notify_subscribed_items
+      if URI(url).host == "hooks.slack.com" || url.ends_with?("/slack")
+        notify_subscribed_items_to_slack
+      else
+        notify_subscribed_items
+      end
     when "my_pawprints"
       notify_pawprints
     end
@@ -59,5 +65,63 @@ class NotificationWebhook < ApplicationRecord
     }
 
     touch(:last_notified_at)
+  end
+
+  def notify_subscribed_items_to_slack(since: nil)
+    at = since || last_notified_at || 6.hours.ago
+    items = user.subscribed_items.preload(:channel).where("items.created_at >= ?", at)
+    return if items.empty?
+
+    items.group_by(&:channel).sort_by { |_, items| items.map(&:created_at).max }.each { |channel, sub_items|
+      blocks = build_blocks(channel, sub_items)
+
+      sleep 2
+      Faraday.post(
+        url, { blocks:, unfurl_links: false }.to_json, "Content-Type" => "application/json"
+      )
+    }
+
+    touch(:last_notified_at)
+  end
+
+  def build_blocks(channel, items)
+    items_blocks =
+      items.sort_by(&:id).reverse.take(4).map { |item|
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "<#{item.url}|#{item.title}>\n#{item.published_at.strftime("%Y-%m-%d %H:%M")}"
+          },
+          accessory: {
+            type: "image",
+            image_url: item.image_url,
+            alt_text: item.title,
+          }
+        }
+      }
+
+    items_blocks.push(
+       {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Check out more recent items in <%s|#{channel.title}>" % [
+              Rails.application.credentials.google_auth_app.host + channel_path(channel)
+            ]
+          }
+       }
+    ) if items.size > 4
+
+    [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "<#{channel.site_url}|#{channel.title}> 's recent items 📨"
+        }
+      },
+      *items_blocks
+    ]
   end
 end
