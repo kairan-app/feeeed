6a4ba5b6d132aaf00a9d87aed4023b3d42272fbc
juneboku <juneboku@gmail.com>
2024-04-22 23:21:39 +0900

    Production環境用のSMTPサーバの設定を施す

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,9 +73,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        ENV["MAIL_SMTP_SERVER"],
+    port:           ENV["MAIL_SMTP_PORT"],
+    user_name:      ENV["MAIL_SMTP_USER"],
+    password:       ENV["MAIL_SMTP_PASSWORD"],
+    domain:         ENV["MAIL_DOMAIN"]
+    authentication: :plain,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
