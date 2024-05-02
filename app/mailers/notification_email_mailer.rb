c82377d6b7f36402cc384f222315d6b112dde4f0
juneboku <juneboku@gmail.com>
2024-05-02 23:11:40 +0900

    NotificationEmailで新着Itemを通知する

--- app/mailers/notification_email_mailer.rb
+++ app/mailers/notification_email_mailer.rb
@@ -4,6 +4,17 @@ class NotificationEmailMailer < ApplicationMailer
     @user = notification_email.user
     @verification_url = notification_email_verification_url(token: notification_email.verification_token)
 
-    mail(to: @notification_email.email, subject: "feeeed")
+    mail(to: @notification_email.email, subject: "Please verify your email address")
+  end
+
+  def subscribed_items(notification_email:, channel_and_items:, subject:)
+    return unless notification_email.verified?
+    return if channel_and_items.empty?
+
+    @notification_email = notification_email
+    @channel_and_items = channel_and_items
+    @user = notification_email.user
+
+    mail(to: @notification_email.email, subject: subject)
   end
 end
--- app/models/notification_email.rb
+++ app/models/notification_email.rb
@@ -9,15 +9,54 @@ class NotificationEmail < ApplicationRecord
   before_validation :set_verification_token
   after_create_commit { NotificationEmailMailer.please_verify(self).deliver_later }
 
+  scope :only_verified, -> { where.not(verified_at: nil) }
+
   enum mode: {
     my_subscribed_items: 0,
     my_pawprints: 1,
   }
 
-  scope :verified, -> { where.not(verified_at: nil) }
+  class << self
+    def notify
+      only_verified.find_each { NotificationEmailNotifierJob.perform_later(_1.id) }
+    end
+  end
 
-  def verify!
-    update!(verified_at: Time.current)
+  def notify
+    case mode
+    when "my_subscribed_items"
+      notify_subscribed_items
+    when "my_pawprints"
+      notify_pawprints
+    end
+  end
+
+  def notify_subscribed_items(since: nil)
+    at = since || last_notified_at || 6.hours.ago
+    items = user.subscribed_items.preload(:channel).where("items.created_at >= ?", at).order("items.id")
+
+    return if items.empty?
+
+    channels = items.map(&:channel).uniq
+
+    subject =
+      if channels.count == 1
+        "New item from #{channels.first.title}"
+      elsif channels.count == 2
+        titles = channels.shuffle.map(&:title).join("、")
+        "New items from #{titles}"
+      else
+        titles = channels.sample(2).map(&:title).join("、")
+        "New items from #{titles} and more"
+      end
+
+    channel_and_items = items.group_by(&:channel).sort_by { |_, items| items.map(&:created_at).max }
+
+    NotificationEmailMailer.subscribed_items(notification_email: self, channel_and_items:, subject:).deliver_later
+  end
+
+  def verified?
+    !verified_at.nil?
   end
 
   def set_verification_token
