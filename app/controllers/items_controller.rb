76e01e1b3eac395c8dc3f7b7332acc8a4e49cb95
juneboku <juneboku@gmail.com>
2024-05-06 21:13:39 +0900

    約数が多めの数字にしておこ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   def index
     page = (params[:page].presence || 1).to_i
-    @items = Item.preload(:pawprints).eager_load(:channel).order(published_at: :desc, title: :desc).page(page)
+    @items = Item.preload(:pawprints).eager_load(:channel).order(published_at: :desc, title: :desc).page(page).per(48)
 
     @title = page == 1 ? "Items" : "Items (Page #{page})"
   end
