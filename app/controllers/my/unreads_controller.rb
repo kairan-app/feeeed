bd57ff9609599819d8bb4a607fd46439c6f4ac7b
juneboku <juneboku@gmail.com>
2024-06-05 09:37:15 +0900

    Unreadsにaudio/video/textのフィルタを実装する

--- app/controllers/my/unreads_controller.rb
+++ app/controllers/my/unreads_controller.rb
@@ -1,17 +1,10 @@
 class My::UnreadsController < MyController
   def show
+    mode = (params[:mode].in?(%w[text audio video]) ? params[:mode] : nil)&.to_sym
     @range_days = (params[:range_days].presence || session[:range_days] || 3).to_i
-    session[:range_days] = @range_days
+    @channel_and_items = current_user.unread_items_grouped_by_channel(range_days: @range_days, mode: mode)
 
-    @channel_and_items =
-      current_user.subscribed_items.
-      preload(:channel).
-      where("NOT EXISTS (SELECT 1 FROM pawprints WHERE pawprints.item_id = items.id AND pawprints.user_id = ?)", current_user.id).
-      where("NOT EXISTS (SELECT 1 FROM item_skips WHERE item_skips.item_id = items.id AND item_skips.user_id = ?)", current_user.id).
-      where("items.created_at > ?", @range_days.days.ago).
-      group_by(&:channel).
-      sort_by { |_, items| items.map(&:created_at).max }.
-      reverse
+    session[:range_days] = @range_days
 
     @title = "Unreads"
   end
