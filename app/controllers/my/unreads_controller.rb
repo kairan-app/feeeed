84071fa5ec009201b35efbc3857d203c33d3b359
juneboku <juneboku@gmail.com>
2024-06-05 09:44:04 +0900

    モードの表示をわかりやすく

--- app/controllers/my/unreads_controller.rb
+++ app/controllers/my/unreads_controller.rb
@@ -1,11 +1,12 @@
 class My::UnreadsController < MyController
   def show
-    mode = (params[:mode].in?(%w[text audio video]) ? params[:mode] : nil)&.to_sym
     @range_days = (params[:range_days].presence || session[:range_days] || 3).to_i
-    @channel_and_items = current_user.unread_items_grouped_by_channel(range_days: @range_days, mode: mode)
+    @mode = params[:mode].in?(%w[text audio video]) ? params[:mode].to_sym : nil
+    @channel_and_items = current_user.unread_items_grouped_by_channel(range_days: @range_days, mode: @mode)
 
     session[:range_days] = @range_days
 
     @title = "Unreads"
+    @title += " #{@mode}s" if @mode
   end
 end
