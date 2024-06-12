40d1874b3c80381555c75af3043dc45ccd914e60
juneboku <juneboku@gmail.com>
2024-06-12 20:24:51 +0900

    細部の気になったところに手を入れる

--- app/controllers/channel_groupings_controller.rb
+++ app/controllers/channel_groupings_controller.rb
@@ -2,9 +2,9 @@ class ChannelGroupingsController < ApplicationController
   before_action :login_required
 
   def create
-    cg = ChannelGrouping.create(channel_id: params[:channel_id], channel_group_id: params[:channel_group_id])
+    grouping = ChannelGrouping.create(channel_id: params[:channel_id], channel_group_id: params[:channel_group_id])
 
-    DiscoPosterJob.perform_later(content: "@#{current_user.name} added #{cg.channel.title} to #{cg.channel_group.name}")
-    redirect_to cg.channel, notice: "Channel added to group"
+    DiscoPosterJob.perform_later(content: "@#{current_user.name} added #{grouping.channel.title} to #{grouping.channel_group.name}")
+    redirect_to grouping.channel, notice: "Channel added to group"
   end
 end
