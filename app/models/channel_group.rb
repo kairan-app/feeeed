dcf7c0cc37c1a11f62255fc6e6b19e4c80ecc843
juneboku <juneboku@gmail.com>
2024-06-22 21:29:23 +0900

    Discordへの通知処理は、たぶんこんな感じ

--- app/models/channel_group.rb
+++ app/models/channel_group.rb
@@ -1,6 +1,8 @@
 class ChannelGroup < ApplicationRecord
   has_many :channel_groupings, dependent: :destroy
   has_many :channels, through: :channel_groupings
+  has_many :items, through: :channels
+  has_many :channel_group_webhooks, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 64 }
 end
--- app/models/channel_group_webhook.rb
+++ app/models/channel_group_webhook.rb
@@ -3,4 +3,20 @@ class ChannelGroupWebhook < ApplicationRecord
 
   validates :channel_group_id, presence: true
   validates :url, presence: true, length: { maximum: 2083 }, format: { with: URI.regexp }
+
+  def notify_items_to_discord(since: nil)
+    at = since || last_notified_at || 6.hours.ago
+    items = channel_group.items.where('created_at > ?', at).order(:created_at)
+    return if items.empty?
+
+    items.group_by(&:channel).sort_by { |_, items| items.map(&:created_at).max }.each do |channel, sub_items|
+      content = "#{channel.title} 's new items 📨"
+      embeds = sub_items.sort_by(&:id).reverse.take(3).map(&:to_discord_embed)
+
+      sleep 2
+      Faraday.post(
+        url, { content:, embeds: }.to_json, "Content-Type": "application/json"
+      )
+    end
+  end
 end
