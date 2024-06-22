8ac8b37baa3eb58c401b8ee1bfca9bf5a28df0a6
juneboku <juneboku@gmail.com>
2024-06-22 22:21:54 +0900

    これで期待通りのDiscord通知になった

--- app/models/channel_group_webhook.rb
+++ app/models/channel_group_webhook.rb
@@ -6,17 +6,19 @@ class ChannelGroupWebhook < ApplicationRecord
 
   def notify_items_to_discord(since: nil)
     at = since || last_notified_at || 6.hours.ago
-    items = channel_group.items.where('created_at > ?', at).order(:created_at)
+    items = channel_group.items.where("items.created_at >= ?", at)
     return if items.empty?
 
     items.group_by(&:channel).sort_by { |_, items| items.map(&:created_at).max }.each do |channel, sub_items|
       content = "#{channel.title} 's new items 📨"
-      embeds = sub_items.sort_by(&:id).reverse.take(3).map(&:to_discord_embed)
+      embeds = sub_items.sort_by(&:id).last(3).map(&:to_discord_embed)
 
       sleep 2
       Faraday.post(
         url, { content:, embeds: }.to_json, "Content-Type": "application/json"
       )
     end
+
+    touch(:last_notified_at)
   end
 end
