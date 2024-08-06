dbc6a67ab7a10cea9f8281fc2330753cb2c41453
juneboku <juneboku@gmail.com>
2024-08-06 22:16:12 +0900

    Inboxページを廃止する

--- app/controllers/my/inbox_controller.rb
+++
@@ -1,7 +0,0 @@
-class My::InboxController < MyController
-  def show
-    @items = current_user.subscribed_items.preload(:pawprints).eager_load(:channel).order(published_at: :desc, title: :desc).page(params[:page])
-
-    @title = "Inbox"
-  end
-end
