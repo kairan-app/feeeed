c561693576f9ee00f6062084a3eb3d1a8aa7c56a
hiromisugie <lozz.gggg@gmail.com>
2024-11-06 09:21:06 +0900

    Channel Groupsの表示コンポーネントを修正してTOPページにも表示

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,17 @@
 class WelcomeController < ApplicationController
   def index
+    @channel_groups =
+      ChannelGroup.
+      all.
+      order(created_at: :desc).
+      limit(5)
     @channels =
       Channel.
         joins(:items).
         select("channels.*, MAX(items.id) AS max_item_id").
         group("channels.id").
         order("max_item_id DESC").
-        limit(3)
+        limit(5)
 
     @title = "Enjoy feeds!"
   end
