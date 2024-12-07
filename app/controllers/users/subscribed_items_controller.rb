43b10d5dd67260650a9c68622ac660a19be1e558
juneboku <juneboku@gmail.com>
2024-12-08 00:06:51 +0900

    ひとつのChannelにつきItemは5個まで、という制御を入れる

--- app/controllers/users/subscribed_items_controller.rb
+++ app/controllers/users/subscribed_items_controller.rb
@@ -1,7 +1,16 @@
 class Users::SubscribedItemsController < ApplicationController
   def index
     @user = User.find_by(name: params[:user_name])
-    @subscribed_items = @user.subscribed_items.order(id: :desc).limit(60)
+    @subscribed_items = Item.find_by_sql([<<~SQL, @user.subscribed_channels.ids])
+      SELECT * FROM (
+        SELECT items.*, ROW_NUMBER() OVER (PARTITION BY channel_id ORDER BY items.id DESC) AS row_number
+        FROM items
+        WHERE items.channel_id IN (?)
+      ) AS numbered_items
+      WHERE row_number <= 5
+      ORDER BY id DESC
+      LIMIT 60
+    SQL
 
     respond_to do |format|
       format.atom
