f5c3e4ee46db95f7a36fad3b1b1963770d0cf901
juneboku <juneboku@gmail.com>
2025-01-25 09:53:45 +0900

    CIでテストが失敗するか試す

--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class ChannelTest < ActiveSupport::TestCase
   test "should not save channel without title" do
     channel = Channel.new
-    assert_not channel.save, "保存されてはいけません"
+    assert(channel.save, "保存されてはいけません")
   end
 end 
