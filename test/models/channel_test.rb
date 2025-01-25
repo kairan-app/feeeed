cd7ef2cd7d1af77b0c3150d5a344d29b88bcbb71
juneboku <juneboku@gmail.com>
2025-01-25 09:58:07 +0900

    細かいところを整える

--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class ChannelTest < ActiveSupport::TestCase
   test "should not save channel without title" do
     channel = Channel.new
-    assert(channel.save, "保存されてはいけません")
+    assert_not(channel.save, "保存に失敗する")
   end
 end 
