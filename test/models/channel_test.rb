5631047978d1c37f6dce418a9bcf9ea3431c7e9d
juneboku <juneboku@gmail.com>
2025-04-29 22:36:27 +0900

    Channel追加のテストを書いていきたい

--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -1,8 +1,24 @@
 require "test_helper"
+require "ostruct"
 
 class ChannelTest < ActiveSupport::TestCase
+  setup do
+    @feed_xml = File.read(Rails.root.join("test/fixtures/juneboku_nikki.xml"))
+    OpenGraph.stubs(:new).returns(OpenStruct.new(image: nil))
+  end
+
   test "should not save channel without title" do
     channel = Channel.new
     assert_not(channel.save, "保存に失敗する")
+  end
+
+  test "フィードのタイトルを正しく取得できる" do
+    channel = Channel.add_by_xml(@feed_xml)
+    assert_equal "juneboku", channel.title
+  end
+
+  test "フィードのURLを正しく取得できる" do
+    channel = Channel.add_by_xml(@feed_xml)
+    assert_equal "https://junebako.github.io/sff/juneboku/nikki.xml", channel.feed_url
   end
 end
