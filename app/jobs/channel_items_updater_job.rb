00ec05b06651f8eba756544be8ebad0b8b1cae7b
juneboku <juneboku@gmail.com>
2025-07-04 00:24:12 +0900

    Channelごとに新着Itemチェックのインターバルを設定できるように

--- app/jobs/channel_items_updater_job.rb
+++ app/jobs/channel_items_updater_job.rb
@@ -3,5 +3,6 @@ class ChannelItemsUpdaterJob < ApplicationJob
     channel = Channel.find(channel_id)
     channel.update_info
     channel.fetch_and_save_items(mode)
+    channel.mark_items_checked!
   end
 end
