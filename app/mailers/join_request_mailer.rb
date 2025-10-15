f1fb2e0a43289ac3ff5565bd688eb12e7e90f728
juneboku <juneboku@gmail.com>
2025-10-15 10:49:33 +0900

    welcomeメールに利用ガイドのリンクを載せる

--- app/mailers/join_request_mailer.rb
+++ app/mailers/join_request_mailer.rb
@@ -2,6 +2,7 @@ class JoinRequestMailer < ApplicationMailer
   def welcome(join_request)
     @join_request = join_request
     @root_url = root_url
+    @guides_url = guides_url
 
     mail(
       to: @join_request.email,
