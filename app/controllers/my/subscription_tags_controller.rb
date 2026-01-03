9069751c6406192e20e579f276e1de09abc363fb
juneboku <juneboku@gmail.com>
2026-01-03 20:40:02 +0900

    管理ページのSubscriptionTagの操作を整える

--- app/controllers/my/subscription_tags_controller.rb
+++ app/controllers/my/subscription_tags_controller.rb
@@ -3,10 +3,7 @@ class My::SubscriptionTagsController < MyController
     @subscription_tag = current_user.subscription_tags.build(subscription_tag_params)
 
     if @subscription_tag.save
-      respond_to do |format|
-        format.html { redirect_to my_subscriptions_path, notice: "Tag created." }
-        format.turbo_stream
-      end
+      redirect_to my_subscriptions_path
     else
       redirect_to my_subscriptions_path, alert: @subscription_tag.errors.full_messages.join(", ")
     end
@@ -16,10 +13,7 @@ class My::SubscriptionTagsController < MyController
     @subscription_tag = current_user.subscription_tags.find(params[:id])
 
     if @subscription_tag.update(subscription_tag_params)
-      respond_to do |format|
-        format.html { redirect_to my_subscriptions_path, notice: "Tag updated." }
-        format.turbo_stream
-      end
+      redirect_to my_subscriptions_path
     else
       redirect_to my_subscriptions_path, alert: @subscription_tag.errors.full_messages.join(", ")
     end
@@ -28,11 +22,7 @@ class My::SubscriptionTagsController < MyController
   def destroy
     @subscription_tag = current_user.subscription_tags.find(params[:id])
     @subscription_tag.destroy
-
-    respond_to do |format|
-      format.html { redirect_to my_subscriptions_path, notice: "Tag deleted." }
-      format.turbo_stream
-    end
+    redirect_to my_subscriptions_path
   end
 
   def reorder
