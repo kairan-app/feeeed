47aca98cf6858bc09fed90fefd60ed83c3a84c1c
juneboku <juneboku@gmail.com>
2026-01-04 20:26:56 +0900

    管理者向け画面にSubscriptionTagの情報を追加する

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,7 @@ class Admin::UsersController < AdminController
     @users = User
       .select("users.*, MAX(ahoy_events.time) AS last_event_at")
       .joins("LEFT JOIN ahoy_events ON ahoy_events.user_id = users.id")
+      .includes(subscription_tags: :subscription_taggings)
       .group("users.id")
       .order("last_event_at DESC NULLS LAST")
     @title = "Admin Users"
