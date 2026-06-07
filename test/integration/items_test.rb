6cee609722b6e6d0d7961d403b175f99392552ca
juneboku <juneboku@gmail.com>
2026-06-08 08:28:27 +0900

    feat(items): 未来日時のItemは一覧に表示しない
    1ヶ月の猶予を持たせる方針から、現在時刻より未来のものは
    すべて表示しない方針に変更。割り切ってシンプルにする。

--- test/integration/items_test.rb
+++ test/integration/items_test.rb
@@ -1,21 +1,21 @@
 require "test_helper"
 
 class ItemsTest < ActionDispatch::IntegrationTest
-  test "shows items published within one month from now" do
-    item = create(:item, title: "Near Future Item", published_at: 3.weeks.from_now)
+  test "shows items published in the past" do
+    item = create(:item, title: "Past Item", published_at: 1.hour.ago)
 
     get items_path
 
     assert_response :success
-    assert_match(/Near Future Item/, response.body)
+    assert_match(/Past Item/, response.body)
   end
 
-  test "does not show items published more than one month from now" do
-    item = create(:item, title: "Far Future Item", published_at: 2.months.from_now)
+  test "does not show items published in the future" do
+    item = create(:item, title: "Future Item", published_at: 1.day.from_now)
 
     get items_path
 
     assert_response :success
-    assert_no_match(/Far Future Item/, response.body)
+    assert_no_match(/Future Item/, response.body)
   end
 end
